bb5fcd5ed737887799bd797daba5e42efcc04f9f
sebastian <sebastian99_19@hotmail.com>
2023-01-30 18:58:42 -0500

    fix api gateway

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require('express')
-const app = express()
+require('./database/mongodb')
 const cors = require('cors')
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./graphql/schema')
+// const userRouter = require('./v1/routes/userRouter')
+// const noteRouter = require('./v1/routes/noteRouter')
+// const error404Handler = require('./middleware/notFound')
 
+// app config
+const app = express()
+const PORT = process.env.PORT || 8000
+
+// middlewars
 app.use(cors())
 app.use(express.json())
 
-const PORT = process.env.PORT || 8000
+// API routes
+app.get('/', (req, res) => res.json({ msg: 'Graphql API' }))
 
+// Error
+// app.use(error404Handler)
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+)
+
+// Server listen
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
