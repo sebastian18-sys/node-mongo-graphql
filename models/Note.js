2ea702061bb30f55c0ef617e4ae00e0719edfa92
sebastian <sebastian99_19@hotmail.com>
2023-01-30 18:57:53 -0500

    fix note model

--- models/Note.js
+++ models/Note.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose')
 const noteSchema = new Schema({
   content: String,
   date: Date,
-  important: true,
+  important: Boolean,
   user: {
     type: Schema.Types.ObjectId,
     ref: 'User'
